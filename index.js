b70d0ff3fa5565b4078960553b27e93ed589774c
smrabiulislam <smarbiulislam03@gmail.com>
2022-10-26 23:49:03 +0600

    json file added

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const port = process.env.port || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json');
 const course = require('./data/course.json');
 
 
@@ -13,19 +12,18 @@ app.get('/', (req, res) => {
     res.send('this is server api')
 });
 
-app.get('/course-categories', (req, res) => {
-    res.send(categories);
-});
 
 app.get('/course', (req, res) => {
     res.send(course);
 });
 
+
 app.get('/course/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedNews = news.find(n => n._id === id);
-    res.send(selectedNews);
-});
+    const id = parseInt(req.params.id);
+    const selectedCourse = course.find(c => c.id === id);
+    res.send(selectedCourse)
+
+})
 
 app.listen(port, () => {
     console.log('api running on server site', port)
